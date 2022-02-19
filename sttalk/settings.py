f7362f6554f1948d0412fa7a975f458941f369cb
5witchkr <hob4410@gmail.com>
2022-02-20 03:22:44 +0900

    setting

--- sttalk/settings.py
+++ sttalk/settings.py
@@ -126,8 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
