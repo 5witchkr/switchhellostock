677d59bb341985dee38845db16d14953b1a4d719
5witchkr <hob4410@gmail.com>
2022-02-20 00:37:51 +0900

    a

--- sttalk/settings.py
+++ sttalk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wpw9$h6%lmd7147k*$#1=cj%59&$m-t_snc8h%(pf=d4ux5gak'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
